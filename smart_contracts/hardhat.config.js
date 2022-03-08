34aafc2c0650c3a5b61f2c1c781336e5382a59c7
LucasHBG <lukahbg@gmail.com>
2022-03-08 19:36:24 -0300

    feat: added alchemy api url & account key

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -1,21 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    ropsten: {
+      //Alchemy API to manage contracts
+      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
+      //Demo Testnet account private key. No problem on being exposed
+      accounts: ['e8911ebcbade1138529496931da18a7959a4ebc9a3865afd7e6b93e23b65b07e'],
+    },
+  }
 };
